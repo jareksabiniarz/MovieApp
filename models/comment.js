df721ed4a96396f0971453c2b85394f90813bfb4
jareksabiniarz <42607008+jareksabiniarz@users.noreply.github.com>
2018-09-12 15:04:28 +0200

    Update and rename comment.js to models/comment.js
    Move file to proper folder

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 // create a new schema- structure of data object
 let commentSchema = new Schema({
-	//referres to Movie model
+//referres to Movie model
     Movie: { type: Schema.Types.ObjectId, ref: "Movie" },	
     Text: String
 });
