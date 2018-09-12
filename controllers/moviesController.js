648cc834534da14c5a1341a0973dbf5580279313
jareksabiniarz <42607008+jareksabiniarz@users.noreply.github.com>
2018-09-12 15:03:46 +0200

    Update and rename moviesController.js to controllers/moviesController.js
    Move file to proper folder

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -6,31 +6,31 @@ const axios = require("axios").default;
 
 router.route('/')
     .get(async (req, res) => {	
-		//create an empty variable to store movie
+	//create an empty variable to store movie
         let movies = null;		
         try {
-			//sort movies from db by title
+	//sort movies from db by title
             if (req.query.sort && req.query.sort === 'true') movies = await Movie.find({}, null, { sort: { Title: 1 } }).populate("Comments").exec();
-			//present movies from db by id if sorting not required
+	//present movies from db by id if sorting not required
             else movies = await Movie.find({}).populate("Comments").exec();	
-			//fetch list of all movies already present in app db in json format instead of an array
+	//fetch list of all movies already present in app db in json format instead of an array
             res.status(200).json(movies);			
         } catch (exception) {
             console.log(exception);
-			//other error
+	//other error
             res.status(500).json("Internal server error");		
         }
     })
     .post(async (req, res) => {
-		//presence of movie title is validated
+	//presence of movie title is validated
         if (!req.body.title) res.status(400).json({ message: "Movie title is required" });
-		//get title from request		
+	//get title from request		
         let title = req.body.title;
-		//get movie from external api		
+	//get movie from external api		
         let movie = (await axios.get("http://www.omdbapi.com/?apikey=7408f029&t=" + title)).data;
-		//create new inctance movie in our app db model 'Movie'		
+	//create new inctance movie in our app db model 'Movie'		
         let saved = await Movie.create(movie);
-		//returns just saved new instance		
+	//returns just saved new instance		
         res.status(200).json(saved);		
     });
 
